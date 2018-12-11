01fcaa061239e3820aed60720f399b0ed2d50851
rb7brady <rb7brady@gmail.com>
2018-12-10 18:47:09 -0600

    forgot some sick files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { StockModule} from './stock/stock.module';
+
 import { AppComponent } from './app.component';
-import { CompanyDetailComponent } from './companies/company-detail.component';
-import { CompanyListComponent } from './companies/company-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CompanyDetailComponent,
-    CompanyListComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'companies/:symbol',
-        component: CompanyDetailComponent },
-      { path: 'companies', component: CompanyListComponent },
-      { path: '', redirectTo: 'companies', pathMatch: 'full' }
-    ])
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    StockModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -1,10 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {CompanyDetailComponent} from './companies/company-detail.component';
+import {CompanyListComponent} from './companies/company-list.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CompanyDetailComponent,
+    CompanyListComponent],
   imports: [
-    CommonModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forChild([
+      { path: 'companies', component: CompanyListComponent },
+      {
+        path: 'companies/:symbol',
+        component: CompanyDetailComponent
+      },
+      { path: '', redirectTo: 'companies', pathMatch: 'full' }
+    ])
   ]
 })
 export class StockModule { }
